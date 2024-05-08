705096dc6dcc7f4a33340be8a540a56c1b0d6829
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-08 14:47:53 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { localQuotes } from './quote';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'quote-generator';
+
+
+  // apiQuotes = [];
+  showLoader = false;
+  showQuoteContainer = true;
+  authorText = '';
+  quoteText = '';
+  longQuote = false;
+
+  newQuote() {
+    const quote: any = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    if (!quote.author) {
+      this.authorText = 'Unknown';
+    } else {
+      this.authorText = quote.author;
+    }
+    this.longQuote = quote.text.length > 120;
+
+    this.quoteText = quote.text;
+  }
+
+
+  ngOnInit(): void {
+    this.newQuote();
+  }
+
+  tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${this.quoteText} - ${this.authorText}`;
+    window.open(twitterUrl, '_blank');
+  }
 }
